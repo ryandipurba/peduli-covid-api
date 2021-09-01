ecebb1e466aa46c086f6546694847106aac4b81b
Ryandi Pratama Purba <purbaryandi@gmail.com>
2021-09-01 17:11:46 +0700

    add update post

--- controllers/help-post-controller.js
+++ controllers/help-post-controller.js
@@ -64,3 +64,38 @@ exports.getHelpPostById = (req, res, next) => {
       console.log(result)
     })
 };
+
+exports.updatePost = (req, res, next) => {
+  const { name, pekerjaan, alamat, provinsi, deskripsi, kebutuhan, norek, image, sosmed, userId } = req.body
+  const postId = req.params.postId
+  helpPosts.findById(postId)
+    .then(result => {
+      if (!result) {
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+        console.log(eror);
+      }
+
+      result.name = name
+      result.userId = userId
+      result.pekerjaan = pekerjaan
+      result.alamat = alamat
+      result.provinsi = provinsi
+      result.deskripsi = deskripsi
+      result.kebutuhan = kebutuhan
+      result.norek = norek
+      result.image = image
+      result.sosmed = sosmed
+      result.status = false
+
+      return result.save()
+    })
+    .then(result => {
+      res.status(200).json({
+        message: "update succes",
+        data: result
+      })
+    })
+    .catch(err => {
+      next(err)
+    })
+}
--- routes/help-post.js
+++ routes/help-post.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getAllPost, createPost, getHelpPostById } = require('../controllers/help-post-controller')
+const { getAllPost, createPost, getHelpPostById, updatePost } = require('../controllers/help-post-controller')
 
 router.get('/posts', getAllPost)
 router.post('/create-post', createPost)
 router.get('/post/:postId', getHelpPostById);
+router.put('/post/:postId', updatePost);
 
 module.exports = router
 
